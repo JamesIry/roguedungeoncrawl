df1a5231b7852b068899968c5a8dd610150c2044
James Iry <jamesiry@gmail.com>
2023-03-26 22:00:48 -0700

    Fix to make dijkstra map use a priority queue instead of a fifo qeueu. Should be marginally faster and significantly less embarrassing

--- src/geometry/dijkstra.rs
+++ src/geometry/dijkstra.rs
@@ -1,7 +1,7 @@
 use crate::prelude::*;
 
 use float_ord::FloatOrd;
-use std::{collections::VecDeque, f32::MAX};
+use std::{collections::BinaryHeap, f32::MAX};
 
 pub struct DijkstraMap {
     pub map: Vec<f32>,
@@ -22,15 +22,16 @@ impl DijkstraMap {
     }
 
     fn build(&mut self, starts: &[usize], map: &Map, max_depth: f32) {
-        let mut queue: VecDeque<(usize, f32)> = starts.iter().map(|p| (*p, 0.0)).collect();
-        while let Some((index, distance)) = queue.pop_front() {
+        let mut queue: BinaryHeap<QueueEntry> =
+            starts.iter().map(|p| QueueEntry(*p, 0.0)).collect();
+        while let Some(QueueEntry(index, distance)) = queue.pop() {
             if distance <= max_depth && distance < self.map[index] {
                 self.map[index] = distance;
                 let exits = map.get_available_exits(index);
                 exits
                     .iter()
-                    .map(|(index, cost)| (*index, distance + cost))
-                    .for_each(|exit| queue.push_back(exit));
+                    .map(|(index, cost)| QueueEntry(*index, distance + cost))
+                    .for_each(|exit| queue.push(exit));
             }
         }
     }
@@ -42,5 +43,26 @@ impl DijkstraMap {
             .filter(|(_, distance)| *distance < MAX)
             .min_by_key(|(_, distance)| FloatOrd(*distance))
             .map(|(index, _)| index)
+    }
+}
+
+struct QueueEntry(usize, f32);
+impl Eq for QueueEntry {}
+impl PartialEq for QueueEntry {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0 && FloatOrd(self.1) == FloatOrd(other.1)
+    }
+}
+
+// Compare "backwards" because the queue want to sort max first
+// where we want min first
+impl Ord for QueueEntry {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        FloatOrd(other.1).cmp(&FloatOrd(self.1))
+    }
+}
+impl PartialOrd for QueueEntry {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
     }
 }
